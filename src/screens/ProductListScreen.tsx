fda6e34697f92d807f2757793715e02591b80ee4
yeonna18k <yeonna18k@gmail.com>
2025-09-14 18:40:18 +0900

    feat: 상품 리스트 화면, 리스트 컴포넌트 구현

---
+++ src/screens/ProductListScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProductList from '../components/product/ProductList';
+
+const ProductListScreen = () => {
+  return <ProductList />;
+};
+
+export default ProductListScreen;
